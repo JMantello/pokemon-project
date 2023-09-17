883781b5b7690f42ab2cdbb5545838c157403f11
Jonathan Mantello <38162257+JMantello@users.noreply.github.com>
2023-09-17 14:34:50 -0700

    add grid and search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import Card from '@mui/material/Card';
@@ -9,79 +7,165 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Item from '@mui/material/Grid';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
 
 function App() {
-  const [count, setCount] = useState(0)
   const [pokemon, setPokemon] = useState(null)
 
-  const fetchPokemonData = async () => {
+  const fetchPokemon = async () => {
     try {
       const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-      const pokemonData = await response.json();
-      setPokemon(pokemonData)
-      console.log(pokemonData)
+      const json = await response.json()
+      setPokemon(json.results)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const fetchSinglePokemon = async (url) => {
+    try {
+      const response = await fetch(url)
+      const json = await response.json()
+      return json
     } catch (e) {
       console.log(e)
     }
   }
 
   useEffect(() => {
-    fetchPokemonData()
+    fetchPokemon()
   }, [])
 
-  function pokemonCard(pokemonObject) {
+  function pokemonItem(p) {
     return (<>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-          sx={{ height: 140 }}
-          image=""
-          title={pokemonObject.name}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {pokemonObject.name}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Share</Button>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
+      <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+        <Item>
+          <Card sx={{ maxWidth: 345, margin: 'auto' }}>
+            <CardMedia
+              sx={{ height: 140 }}
+              image=""
+              title={p.name}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {p.name}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </ Item>
+      </Grid>
     </>)
   }
 
-  function defaultPage() {
+  function pokemonGrid() {
+    return (<>
+      <Grid container spacing={2}>
+        {
+          pokemon.map((p) => (
+            pokemonItem(p)
+          ))
+        }
+      </Grid>
+    </>)
+  }
+
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
+
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }));
+
+  function SearchAppBar() {
     return (
-      <>
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </>
-    )
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            >
+              Pokemon
+            </Typography>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    );
   }
 
   return (
     <>
-      {pokemon.map((p) => (
-        pokemonCard(p)
-      ))}
+      <SearchAppBar />
+      <br />
+      {pokemon ? pokemonGrid() : <h1>Loading...</h1>}
     </>
   )
 }
